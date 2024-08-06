7c27451db0315f0cdbb8a099c6cc04d02be6a830
Skylar Pape <11418651+skylarpape@users.noreply.github.com>
2024-08-05 21:46:16 -0400

    Parses tax wihtholdings from pdf text

--- export_paystubs_to_csv.py
+++ export_paystubs_to_csv.py
@@ -1,5 +1,9 @@
 import pypdf
+import re
 
 def main(args):
     pdf_reader = pypdf.PdfReader(args.paystubs.name) 
-    print(pdf_reader.pages[0].extract_text())
+    page_text = pdf_reader.pages[0].extract_text()
+    if match := re.search(".+Federal Withholding((\d|,)+\.\d\d)State Tax - MA(.+)Earnings.+", page_text):
+        print(match.group(1))
+        print(match.group(2))
