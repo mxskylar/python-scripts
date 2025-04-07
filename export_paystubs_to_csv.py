7ed4d2a937664cf02b48960067042aad78916dab
Skylar <11418651+mxskylar@users.noreply.github.com>
2025-04-07 09:56:27 -0500

    Adds script that exports contents of payslips from PDF to CSV (#1)

--- export_paystubs_to_csv.py
+++ export_paystubs_to_csv.py
@@ -1,2 +1,18 @@
+import pypdf
+import re
+
+OUTPUT_CSV = "output/paystubs.csv"
+
 def main(args):
-    print(args)
+    with open(OUTPUT_CSV, "w") as file:
+        file.write("Federal Withholding,State Withholding")
+    pdf_reader = pypdf.PdfReader(args.paystubs.name)
+    for page in pdf_reader.pages:
+        page_text = page.extract_text()
+        match = re.search("Federal Withholding(((\d|,)+\.\d\d))", page_text)
+        if match:
+            # federal withholding
+            print(match.group(1))
+            #with open(OUTPUT_CSV, "a") as file:
+                #file.write(f"\n{federal_withholding}|{state_withholding}")
+    print(f"✅ Exported paystub data to {OUTPUT_CSV}")
